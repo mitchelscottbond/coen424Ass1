73829c60084229249a800398507fb39ddae583fe
Mitchel Scott-Bond <65721975+mitchelscottbond@users.noreply.github.com>
2022-10-28 13:50:12 -0400

    proto server and Proto client created and functional
    requires pip install google protobuf

---
+++ ProtoClient.py
@@ -0,0 +1,124 @@
+import socket
+import sys
+import json
+import csv
+import numpy as np
+import protoFile_pb2
+
+benchmarkType = ""
+workloadMetric = ""
+global batchunit
+global batchid
+global batchsize
+data_type = ""
+print('Enter the RFW ID:')
+RFWID =  input()
+print("Enter bench mark type: 1 for 'DVD' or enter 2 for 'NDBench' for bench mark type")
+while True:
+    benchmarktemp = input()
+    if benchmarktemp == "1":
+        benchMarkType = "DVD"
+        break
+    elif benchmarktemp =="2":
+        benchMarkType = "NDBench"
+        break
+    else:
+        print("Invalid input for bench mark type, try again:")
+
+
+print("Enter Workload Metric: '1 for = CPUUtilization_Average' '2 = NetworkIn_Average' '3 = NetworkOut_Average' '4 = MemoryUtilization_Average'")
+while True:
+    workloadmet = input()
+    if workloadmet == "1":
+        workloadMetric ="CPU"
+        break
+    elif workloadmet == "2":
+        workloadMetric ="NetworkIn"
+        break
+    elif workloadmet == "3":
+        workloadMetric = "NetworkOut"
+        break
+    elif workloadmet == "4":
+        workloadMetric = "Memory"
+        break
+    else:
+        print("Invalid input for workload metric, try again:")
+
+
+print("Enter batch unit as an integer")
+
+while True:
+    batchun = input()
+    batchunit = int(batchun)
+    if type(batchunit) == int:
+        break
+    else:
+        print("Invalid input for batch unit, try again:")
+
+
+print("Enter batch ID as an integer")
+while True:
+    temp = input()
+    batchid = int(temp)
+    if type(batchid) == int:
+        break
+    else:
+        print("Invalid input for batch unit, try again:")
+
+print("Enter batch size as an integer")
+while True:
+    temp = input()
+    batchsize = int(temp)
+    if type(batchsize) == int:
+        break
+    else:
+        print("Invalid input for batch unit, try again:")
+
+
+print("Enter data type: '1 = testing' '2 = training'")
+
+while True:
+    datatype = input()
+    if datatype == "1":
+        data_type="testing"
+        break
+    elif datatype == "2":
+        data_type ="training"
+        break
+    else:
+        print("Invalid input for workload metric, try again:")
+
+print("Enter data analytics in the format of '99p', or '50p', or '10p', or '70p', or 'avg', or 'std', or 'max', or 'min'\n If one of these values are not entered, the code will not function correctly")
+analytics = input()
+
+proto = protoFile_pb2.RFW() 
+proto.RFWID = RFWID
+proto.benchmarkType = benchMarkType
+proto.workloadMetric = workloadMetric
+proto.batchUnit = batchunit
+proto.batchID = batchid
+proto.batchSize = batchsize
+proto.dataType = data_type
+proto.dataAnalytics = analytics
+
+HOST = "localhost"  # The server's hostname or IP address
+PORT = 50000  # The port used by the server
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    print(proto)
+    msg = proto.SerializeToString()
+    print(msg)
+    sock.send(msg)
+
+    # Receive data from the server and shut down
+    received = sock.recv(1000000000)
+    protoResponse = protoFile_pb2.RFD()
+    response = protoResponse.FromString(received)
+
+finally:
+    sock.close()
+print(f"Received {response!r}")
---
+++ ProtoServer.py
@@ -0,0 +1,136 @@
+import socket
+import csv
+import numpy as np
+import protoFile_pb2
+import json
+
+#encode via latin-1
+HOST = "localhost"  # Standard loopback interface address (localhost)
+PORT = 50000  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1000000000)
+            print(data)
+
+            protoData = protoFile_pb2.RFW()
+            msg = protoData.FromString(data)
+
+            ##getting all my data from the RFW.
+            RFWID = msg.RFWID
+            benchmark_type = msg.benchmarkType
+            workload_metric = msg.workloadMetric
+            batch_unit = msg.batchUnit
+            batch_ID = msg.batchID
+            batch_size = msg.batchSize
+            data_type = msg.dataType
+            data_analytics = msg.dataAnalytics
+            
+            if benchmark_type =="":
+                exit()
+            
+            filename = benchmark_type + '-' + data_type + '.csv'
+            def check_workload_metric(workload_metric):
+                if workload_metric == "CPU":
+                    workload_colum = 0
+                elif workload_metric == "NetworkIn":
+                    workload_colum = 1 
+                elif workload_metric == "NetworkOut":
+                    workload_colum = 2
+                elif workload_metric == "Memory":
+                    workload_colum = 3
+                elif workload_metric == "":
+                    workload_colum = 0
+                return workload_colum
+
+            workload_column = check_workload_metric(workload_metric)
+
+            #calculating batch size as well as specific units to determine first and last element in batch
+            required_return_amount= batch_unit * batch_size
+            first_element_in_batch = batch_unit*(batch_ID-1) 
+            last_element_in_batch = first_element_in_batch + required_return_amount
+            last_batch_ID = batch_size + (batch_ID-1)
+
+            #
+            def retrieve_data_to_spec(filename, first_element_in_batch, last_element_in_batch, workload_column):
+                with open(filename) as csv_file:
+                    #prepping a list to be used later
+                    my_returnable_list = []
+                    #heading = next(csv_file)
+                    csv_reader = csv.reader(csv_file, delimiter=',')
+                    line_count = 0
+                    allRows = list(csv_reader)
+                    interestedRows = allRows[first_element_in_batch+1:last_element_in_batch+1]
+
+                    for row in interestedRows:
+                        my_returnable_list.append(row[workload_column])
+                        line_count+=1
+                
+                    #print("my list of data points is given below. It can be modified to print by new lines or in the format it is currentyl shown by changing the backslah t to a backslah n")
+                    #print(*my_returnable_list, sep = "\t")
+                    #print(f'Processed {line_count} lines.')
+
+                #ensuring data is not type string but type int or float etc...
+                res = [eval(i) for i in my_returnable_list]
+                print("List of data as it's data type is: ", res)
+
+                
+                return res
+
+            data_list = retrieve_data_to_spec(filename, first_element_in_batch, last_element_in_batch, workload_column)
+            sorted_data_list = sorted(data_list)
+            def perform_data_analytics(data_list, data_analytics):
+                a= np.array(data_list)
+                print("printing numpy array in ascending order")
+                print(*a)
+
+                if data_analytics == "avg":
+                    p = np.average(a)
+                    print("average = ")
+                    print(p)
+                elif data_analytics == "std":
+                    p = np.std(a)
+                    print("std = ")
+                    print(p)
+                elif data_analytics == "max":
+                    p = np.max(a)
+                    print("max = ")
+                    print(p)
+                elif data_analytics =="min":
+                    p = np.min(a)
+                    print("min = ")
+                    print(p)
+                else:
+                    temp = int(data_analytics.replace('p', ''))
+                    p = np.percentile(a, temp)
+                    print('else percentile = ')
+                    print( p)
+                return p
+
+            analytics = perform_data_analytics(sorted_data_list, data_analytics)
+            print('results of data analytics')
+            print(analytics)
+
+            RFDmessage = {}
+            RFDmessage["RFWID"]=RFWID
+            RFDmessage["lastBatchID"]= last_batch_ID
+            RFDmessage["dataSamples"] =data_list
+            RFDmessage["dataAnalytics"]=analytics
+
+            proto = protoFile_pb2.RFD() 
+            proto.RFWID = RFWID
+            proto.LastBatchID = last_batch_ID
+            proto.dataRequested.extend(data_list)
+            proto.dataAnalytics = analytics
+
+            response = proto.SerializeToString()
+
+            if not data:
+                break
+                
+            conn.send(response)
